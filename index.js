f26dfd1a8e893e87d50b3a1f62e98a3f4bc68a2c
sabitameem <sabitameem578@gmail.com>
2023-06-03 22:06:45 +0530

    payment intents API to get client Secret from stripe

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app =express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -199,6 +200,43 @@ async function run() {
      const result = await cartCollection.deleteOne(query);
      res.send(result);
   })
+
+
+
+
+  //payment related
+  app.post("/create-payment-intent",async(req,res)=>{
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+
+
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
